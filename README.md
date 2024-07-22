4ca4271e7c835cb90ee62ddbadc033e65f672d0e
OptimisedLEarning <158731825+OptimisedLEarning@users.noreply.github.com>
2024-07-22 17:46:31 +0530

    Calculaor with just minimum functionality

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
 # cal_see
+
+
+
+ step 1  define 4 functions add, sub,mul,div
+
+ var 1st num = 0 ;
+
+ var  2nd num = 0 ;
+
+ var  operator= "" ;
+
+
+ step 2 
+                  initial_value  = 0
+                    function display {
+                    for class numbers
+                    if number buttons   are pressed 
+                        initial_value += number buttons value  
+                        screen_value = initial_value
+                }      
+
+       1st number = [if  at first number buttons are pressed
+                    display on the display bar ]   or  display()
+
+
+   
+     operator variable  =   if  (any of these  * ,+,- ,/  ) are pressed 
+
+    after storing operator  
+    when the number is pressed
+    1ts number =  initial value   
+     clear the previous display screen   and      
+    display the numbers  on the screen as we press those numbers   before pressing equal  store those number in 2nd num    
+    before pressing   
+     equal to sign =    
+
+ ;
+
+ step 3  function  operate(1st num ,operator ,second num){
+
+
+     when equal to is clicked operate function  is called 
+
+     
+
+if equal is pressed  = "="{
+       check for operator value 
+       
+       if operator = "+"{
+        
+      return  display the result [  add(1st ,2nd )]
+      
+                     and so on }
+} 
+
+
+  }
+
+------------------------------------------------
+     initial value  = 0 
+    
+    function display_screen {
+    if numbers are pressed 
+    initial value += numbers
+
+    if any  operator(+-*/) are pressed 
+    store  operator variable
+    first number = initial value
+    if numbers  are pressed 
+    second number +=  i  {
+
+        if equal  is pressed 
+
+        function call operate( ){
+
+               if (1st_num !=0 && operator!=""  and  2nd num= 0){
+
+                     perform  add or sub or mul  
+                               initial value 0
+                                2nd_num=0
+                        
+                        display add ,sub , mul ,div
+
+
+
+               }
+         }
+ 
+
+    }
+ }
+
+ 
+
+
+
+
+
+  
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,170 @@
+function add(a,b){
+    return a+b
+    }
+  
+  function sub(a,b){
+          return a-b 
+  
+  }
+  function mul(a,b){
+      return a*b
+  }
+  function div(a,b){
+   return a/b
+  }
+  
+
+  
+function operate( fir_var, operator ,sec_var){
+   if (operator == "+"){
+          return  add(fir_var,sec_var)
+    }
+      else if (operator == "-"){
+          return sub(fir_var,sec_var)
+    }
+      else  if (operator == "*"){
+           return mul(fir_var,sec_var)
+    }
+      else if (operator == "/" ){
+          return div(fir_var,sec_var)
+      }
+      
+  }
+  
+  
+  
+  window.onload = function () {
+
+    var screen  = document.getElementById('screen');
+    // Set the initial value to 0
+    screen.value = "0";
+    
+    let first_num =0;
+    
+    let second_num =0; 
+    
+    let input_number = 0;   
+
+    let theOperator=""; 
+
+    let operatorClicked =false;
+
+
+    const numbers = document.querySelectorAll(".operand"); 
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', function () {
+
+            if (screen.value === "0" || operatorClicked) {
+                screen.value = this.textContent;
+                operatorClicked =false;
+            } else {
+               screen.value +=  this.textContent;
+            }
+            //parseFloat is used to convert it to numbers  
+            
+            input_number = parseFloat(screen.value);  
+            // Updating the global variable
+            
+            window.input_number = input_number;
+
+
+             if (theOperator != "") {
+               
+                screen.value = input_number  ;
+
+                second_num = parseFloat(screen.value);
+                
+                window.second_num  = second_num;
+                
+            } else {
+                // If no operator is selected, just update  first num  as input_number
+
+                first_num = input_number;
+
+                window.first_num  = first_num;
+                
+
+            }
+        });
+    }
+
+      const operator = document.querySelectorAll(".operator");
+  
+      for ( let i = 0; i < operator.length; i++) {
+          operator[i].addEventListener  ("click",function ()
+          {       
+            
+            if (screen.value !== "0") {
+                input_number = parseFloat(screen.value);
+            }
+               operatorClicked =true;
+
+                theOperator =  this.textContent;
+                
+                first_num = input_number
+
+                   
+            
+                // Updating the global variables
+            window.theOperator = theOperator;
+            window.first_num = first_num;
+            window.input_number = input_number;
+
+                });
+           
+      }; 
+
+   
+    
+      const ac = document.querySelector(".clear");
+      ac.addEventListener("click", function() {
+            operatorClicked =false;
+            screen.value=0;
+            input_number=0
+            theOperator ="";
+            first_num=0; 
+            second_num=0;
+
+             // Update the global variables    
+            window.input_number = input_number;
+            window.theOperator = theOperator;
+            window.first_num =first_num;
+            window.second_num =second_num;
+            
+          
+        },
+       
+      );
+      
+    const equal = document.querySelector(".equals");
+        equal.addEventListener("click", function(){
+                 let result = operate(first_num,theOperator,second_num);
+                screen.value = result
+                // result for future calculations
+                
+                first_num = result; // Store the 
+
+                console.log(`After equals clicked: Result is ${result}`); 
+                
+                window .first_num=first_num;
+            });
+  
+
+
+
+
+
+  }
+  
+ ;
+  
+    
+     
+  ;
+  
+  
+  
+  
+    
+  
+  
